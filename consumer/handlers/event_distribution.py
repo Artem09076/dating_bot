4f13f6efdcd82735a0840e4681013998e0fe4754
nastyazz <nastayz0808@mail.ru>
2025-04-13 12:33:27 +0300

    add action in body

--- consumer/handlers/event_distribution.py
+++ consumer/handlers/event_distribution.py
@@ -3,5 +3,5 @@ from consumer.handlers.create_form import create_form
 
 async def handle_event_distribution(body: Dict[str, Any]) -> None:
     match body['action']:
-        case 'create_form':
+        case 'make_form':
             await create_form(body)
